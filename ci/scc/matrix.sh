4d8aa22371adc70398be2986859118185a1bbf31
Liquid <Liquid369@gmail.com>
2022-11-04 09:22:48 -0500

    dash:4023 remove BIP70 support

--- ci/scc/matrix.sh
+++ ci/scc/matrix.sh
@@ -37,6 +37,7 @@ export RUN_INTEGRATIONTESTS=true
 
 # Configure sanitizers options
 export TSAN_OPTIONS="suppressions=${SRC_DIR}/test/sanitizer_suppressions/tsan"
+export UBSAN_OPTIONS="suppressions=${SRC_DIR}/test/sanitizer_suppressions/ubsan"
 
 if [ "$BUILD_TARGET" = "arm-linux" ]; then
   export HOST=arm-linux-gnueabihf
@@ -53,32 +54,39 @@ elif [ "$BUILD_TARGET" = "win64" ]; then
   export DIRECT_WINE_EXEC_TESTS=true
 elif [ "$BUILD_TARGET" = "linux32" ]; then
   export HOST=i686-pc-linux-gnu
-  export DEP_OPTS="NO_PROTOBUF=1"
-  export BITCOIN_CONFIG="--enable-zmq --disable-bip70 --enable-reduce-exports --enable-crash-hooks"
+  export BITCOIN_CONFIG="--enable-zmq --enable-reduce-exports --enable-crash-hooks"
   export USE_SHELL="/bin/scc"
   export PYZMQ=true
 elif [ "$BUILD_TARGET" = "linux32_ubsan" ]; then
   export HOST=i686-pc-linux-gnu
-  export BITCOIN_CONFIG="--enable-zmq --disable-bip70 --enable-reduce-exports --enable-crash-hooks --with-sanitizers=undefined"
+  export BITCOIN_CONFIG="--enable-zmq --enable-reduce-exports --enable-crash-hooks --with-sanitizers=undefined"
   export USE_SHELL="/bin/scc"
   export PYZMQ=true
 elif [ "$BUILD_TARGET" = "linux64" ]; then
   export HOST=x86_64-unknown-linux-gnu
   export DEP_OPTS="NO_UPNP=1 DEBUG=1"
   export BITCOIN_CONFIG="--enable-zmq --enable-reduce-exports --enable-crash-hooks"
-  export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_scc_DEBUG -DARENA_DEBUG"
+  export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_SCC_DEBUG -DARENA_DEBUG"
   export PYZMQ=true
 elif [ "$BUILD_TARGET" = "linux64_tsan" ]; then
   export HOST=x86_64-unknown-linux-gnu
   export DEP_OPTS="NO_UPNP=1 DEBUG=1"
   export BITCOIN_CONFIG="--enable-zmq --enable-reduce-exports --enable-crash-hooks --with-sanitizers=thread"
-  export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_scc_DEBUG -DARENA_DEBUG"
+  export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_SCC_DEBUG -DARENA_DEBUG"
   export PYZMQ=true
+elif [ "$BUILD_TARGET" = "linux64_fuzz" ]; then
+  export HOST=x86_64-unknown-linux-gnu
+  export DEP_OPTS="NO_UPNP=1 DEBUG=1"
+  export BITCOIN_CONFIG="--enable-zmq --disable-ccache --enable-fuzz --with-sanitizers=fuzzer,address,undefined CC=clang CXX=clang++"
+  export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_SCC_DEBUG -DARENA_DEBUG"
+  export PYZMQ=true
+  export RUN_UNITTESTS=false
+  export RUN_INTEGRATIONTESTS=false
 elif [ "$BUILD_TARGET" = "linux64_cxx20" ]; then
   export HOST=x86_64-unknown-linux-gnu
   export DEP_OPTS="NO_UPNP=1 DEBUG=1"
   export BITCOIN_CONFIG="--enable-zmq --enable-reduce-exports --enable-crash-hooks --enable-c++20 --enable-suppress-external-warnings --enable-werror"
-  export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_scc_DEBUG -DARENA_DEBUG"
+  export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_SCC_DEBUG -DARENA_DEBUG"
   export PYZMQ=true
   export RUN_INTEGRATIONTESTS=false
 elif [ "$BUILD_TARGET" = "linux64_nowallet" ]; then
