3648e4173a21f6fc4cf877a54356b973b8b1aff1
Liquid <Liquid369@gmail.com>
2022-11-10 13:09:13 -0600

    dash:4849 btc backports

--- src/scc-wallet.cpp
+++ src/scc-wallet.cpp
@@ -45,8 +45,8 @@ static bool WalletAppInit(int argc, char* argv[])
     }
     if (argc < 2 || HelpRequested(gArgs)) {
         std::string usage = strprintf("%s scc-wallet version", PACKAGE_NAME) + " " + FormatFullVersion() + "\n\n" +
-                                      "wallet-tool is an offline tool for creating and interacting with SCC Core wallet files.\n" +
-                                      "By default wallet-tool will act on wallets in the default mainnet wallet directory in the datadir.\n" +
+                                      "scc-wallet is an offline tool for creating and interacting with SCC Core wallet files.\n" +
+                                      "By default scc-wallet will act on wallets in the default mainnet wallet directory in the datadir.\n" +
                                       "To change the target wallet, use the -datadir, -wallet and -testnet/-regtest arguments.\n\n" +
                                       "Usage:\n" +
                                      "  scc-wallet [options] <command>\n\n" +
